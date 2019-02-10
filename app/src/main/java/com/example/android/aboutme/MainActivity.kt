6ba465884fa152dc93f35834ddc5faf05a9056c9
Abdul Rozak <rozak@nawatech.co>
2019-02-10 20:39:28 +0700

    learn data binding for data

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -12,20 +12,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Abdul Rozak")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         binding.doneButton.setOnClickListener{
-            addNickname(it)
+            addNickname()
             // Hide the keyboard.
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
     }
 
-    private fun addNickname(view: View){
+    private fun addNickname(){
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = binding.nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
